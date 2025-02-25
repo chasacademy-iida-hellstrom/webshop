c469f16c0d1d4b1f55a7cfb72a45fee4d0bc35fd
iida hellström <iidahellstrom@gmail.com>
2025-02-25 12:10:28 +0100

    routerändring

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import ProductList from "./components/ProductList";  // Importera ProductList
-import ProductPage from "./pages/ProductPage";  // Importera ProductPage
+import ProductList from "./components/ProductList"; 
+import ProductPage from "./pages/ProductPage";  
 
 const AppRouter = () => {
   return (
@@ -10,13 +10,13 @@ const AppRouter = () => {
       <nav>
         <Link to="/">Hem</Link>
         <Link to="/about">Om oss</Link>
-        <Link to="/products">Produkter</Link>  {/* Ny länk för Products */}
+        <Link to="/products">Produkter</Link>  
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/products" element={<ProductList />} />  {/* Ny rutt för ProductList */}
-        <Route path="/product/:id" element={<ProductPage />} />  {/* Ny rutt för ProductPage */}
+        <Route path="/products" element={<ProductList />} />  
+        <Route path="/product/:id" element={<ProductPage />} />  
       </Routes>
     </Router>
   );
