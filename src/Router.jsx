8ca9a889b2a85d8303db4996904bf2814ba923e0
Ouro möllah Farida <farida@MacBook-Air-som-tillhor-Ouro.local>
2025-02-27 16:21:07 +0100

    skapa en searchbar komponent

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import ProductList from "./components/ProductList";
@@ -6,6 +7,7 @@ import ProductPage from "./components/ProductPage";
 import Cart from "./components/Cart";
 import OrderConfirmation from "./components/OrderConfirmation";
 import Navbar from "./components/Navbar";
+import SearchBar from "./components/SearchBar";
 
 const AppRouter = () => {
   return (
@@ -18,16 +20,15 @@ const AppRouter = () => {
           <li><a href="/cart">Kundvagn</a></li>
         </ul>
       </nav>
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/products" element={<ProductList />} />
+        <Route path="/products" element={<ProductList  />} />
         <Route path="/products/:id" element={<ProductPage />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/order-confirmation" element={<OrderConfirmation />} />
       </Routes>
-
+    <SearchBar/>
       <Navbar />
     </Router>
   );
