e2ce53ac465b9fafb2efbe0a331e5433019f9fca
EmNatter <emilia_natter@hotmail.com>
2025-03-02 19:27:34 +0100

    navbar upp-responsiv

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import { HiOutlineShoppingBag } from "react-icons/hi";
 import { MdOutlineSearch } from "react-icons/md";
 import { useState, useEffect, useRef } from "react";
 import { Link, useLocation } from "react-router-dom";
+import Logo from "../Images/logo.svg";
 
 const Navbar = () => {
   const [cartCount, setCartCount] = useState(0);
@@ -56,6 +57,11 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
+      <div className="logo-item">
+          <Link to="/">
+            <img src={Logo} alt="Logo" className="logo" />
+          </Link>
+        </div>
       <ul className="navList">
         <li>
           <Link to="/search">
@@ -68,7 +74,10 @@ const Navbar = () => {
           </Link>
         </li>
         <li>
-          <button className="menu-button" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+          <button
+            className="menu-button"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
             <LuAlignJustify className="navIcons" />
           </button>
         </li>
@@ -90,7 +99,10 @@ const Navbar = () => {
           <ul>
             {categories.map((category) => (
               <li key={category}>
-                <Link to={`/category/${category}`} onClick={() => setIsMenuOpen(false)}>
+                <Link
+                  to={`/category/${category}`}
+                  onClick={() => setIsMenuOpen(false)}
+                >
                   {category}
                 </Link>
               </li>
@@ -103,4 +115,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
